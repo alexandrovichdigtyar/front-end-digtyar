64f8256f0db298fa5d71ad73eb83964c3c59566e
alexandrovichdigtyar <71710384+alexandrovichdigtyar@users.noreply.github.com>
2021-09-16 10:06:11 +0300

    Add files via upload

---
+++ HW26/app.js
@@ -0,0 +1,6 @@
+import * as calculator from './calculator.js';
+
+console.log(calculator.add(1,5))
+console.log(calculator.sub(5,2))
+console.log(calculator.mult(7, 4))
+console.log(calculator.div(28, 2))
---
+++ HW26/calculator.js
@@ -0,0 +1,10 @@
+import {add} from './add.js'
+export {add}
+import {sub} from './sub.js'
+export {sub}
+
+import {mult} from './mult.js'
+export {mult} 
+
+import { div } from './div.js'
+export {div}
